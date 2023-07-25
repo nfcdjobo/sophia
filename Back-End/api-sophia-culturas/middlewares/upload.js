d71b02ade2e46215a8e073d6b037eee4f7c05c6c
nfcdjobo <nfcdjobo@gmail.com>
2023-07-25 17:45:32 +0000

    m

--- Back-End/api-sophia-culturas/middlewares/upload.js
+++ Back-End/api-sophia-culturas/middlewares/upload.js
@@ -4,13 +4,11 @@ const multer = require(`multer`);
 let storage = multer.diskStorage({
     destination: function(req, file, cb){
         cb(null, "stockage/");
-        console.log('stockage/');
     },
     filename: function(req, file, cb){
         let ext = path.extname(file.originalname); //On récupère le nom original de l'image
         const r = al => al.length == 2 ? al[1] : r;
         cb(null, Date.now()+ r(Math.random().toString().split('.')) + ext);
-        console.log(ext);
     }
 })
 
@@ -21,7 +19,6 @@ let Upload = multer({
             || file.mimetype === `image/jpeg` || file.mimetype === `image/webp` || file.mimetype === `image/gif`
             ||  file.mimetype === `image/avif`){
             callback(null, true);
-            console.log("COOL");
         }else{
             callback(null, false);
             console.log(`Seule les image d'extention .png, .svg, .jpg, .gif, .webp, .avif et .jpeg sont recommandés.`);
